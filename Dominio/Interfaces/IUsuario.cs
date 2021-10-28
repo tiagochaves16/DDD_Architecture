036cc167c250e3c95c6b996f39ad34f2fa20bd70
Tiago Chaves <Tiagochaves@live.com>
2021-10-27 21:27:14 -0300

    Commit final WebApiDDD

---
+++ Dominio/Interfaces/IUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Dominio.Interfaces
+{
+    public interface IUsuario
+    {
+        Task<bool> AdicionaUsuario(string email, string senha, int idade, string celular);
+
+        Task<bool> ExisteUsuario(string email, string senha);
+
+        Task<string> RetornaIdUsuario(string email);
+    }
+}
